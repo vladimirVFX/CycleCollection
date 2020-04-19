2a3c242c3d0f7add2bb7e307ad6a76cc11eecf70
VladimirMironov <44780782+vladimirVFX@users.noreply.github.com>
2020-04-19 15:57:56 +0300

    Add files via upload

---
+++ CycleCollection.cpp
@@ -0,0 +1,37 @@
+﻿// CycleCollection.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    system("chcp 1251 > null");
+    srand(2);
+    int nums[12];
+    
+    cout << "Massive random numbers" << "\n";
+    for (int& x : nums) {
+        x = rand() % 10;
+        cout << x << " ";
+    }
+    cout << endl;
+
+    int length = 0;
+    for (int& x : nums) {
+        length++;
+    }
+
+    cout << "Dimension massive" << "\n";
+    cout << "Check massive" << "\n";
+    for (int k = 0; k < length; k++) {
+        cout << nums[k] << " ";
+    }
+
+    cout << endl;
+    system("pause > nul");
+    return 0;
+}
+
